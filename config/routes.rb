5505496d86b01bc38085bcc7330a5b37de4c5d9c
Philco Michel <phil9231@gmail.com>
2016-11-27 14:25:56 -0500

    new page add

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'other/prayer'
   get 'other/saturday'
   get 'other/talk'
+  get 'other/lastsaturday'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
